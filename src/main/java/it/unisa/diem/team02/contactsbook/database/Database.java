2ecaef42b6462d31b1289bed050a7a9a958476c8
angel <angel@MattiaSanzari>
2024-12-06 21:14:05 +0100

    Merge origin/project-nebeans-23-Vittorio into project-nebeans-23-Vittorio
    # Conflicts:
    #	src/main/java/it/unisa/diem/team02/contactsbook/ui/controllers/ContactsbookViewController.java
    aggiunta commenti per la classe Database+ 2 funzionalità

--- src/main/java/it/unisa/diem/team02/contactsbook/database/Database.java
+++ src/main/java/it/unisa/diem/team02/contactsbook/database/Database.java
@@ -29,6 +29,7 @@ import org.mindrot.jbcrypt.BCrypt;
 public class Database  {
         
 /**
+ * @lang it
  * @brief Stabilisce una connessione al database PostgreSQL.
  * 
  * @param dbname Nome del database a cui connettersi.
@@ -36,6 +37,15 @@ public class Database  {
  * @param password Password per accedere al Database .
  * @return Oggetto Connection se la connessione è riuscita, altrimenti null.
  * @throws SQLException Se si verifica un errore durante la connessione.
+ * 
+ * @lang en
+ * @brief Establishes a connection to the PostgreSQL database.
+ *
+ * @param dbname Name of the database to connect to.
+ * @param user Name of the database owner in pgAdmin.
+ * @param password Password to access the database.
+ * @return Connection object if the connection is successful, otherwise null.
+ * @throws SQLException If an error occurs during the connection.
  */
     
     public Connection ConnectionDB(String dbname, String user, String password) {
@@ -61,6 +71,7 @@ public class Database  {
     
     
     /**
+    * @lang it
     * @brief Inserisce un nuovo utente nella tabella specificata.
     * 
     * @param conn Oggetto Connection per interagire con il database.
@@ -68,7 +79,17 @@ public class Database  {
     * @param email Email dell'utente.
     * @param password Password dell'utente (verrà criptata  prima dell'inserimento).
     * @throws SQLException Se si verifica un errore durante l'inserimento.
+    * @lang en
+    * 
+    * @brief Inserts a new user into the specified table.
+    * 
+    * @param conn Connection object to interact with the database.
+    * @param tableName Name of the table in which to insert the user.
+    * @param email Email of the user.
+    * @param password Password of the user (will be encrypted before insertion).
+    * @throws SQLException If an error occurs during the insertion.
     */
+
     public void insertUser(Connection conn, String tableName, String email, String password) throws SQLException{
     
            Statement statment;
@@ -81,12 +102,22 @@ public class Database  {
     }
     
     /**
+    * @lang it
     * @brief Recupera tutti gli utenti dal database.
     * 
     * @param conn Oggetto Connection per interagire con il database.
     * @param tableName Nome della tabella da cui recuperare gli utenti.
     * @return HashMap contenente le coppie email-password degli utenti.
+    * @lang en
+    * 
+    * @brief Retrieves all users from the database.
+    * 
+    * @param conn Connection object to interact with the database.
+    * @param tableName Name of the table from which to retrieve the users.
+    * @return HashMap containing the email-password pairs of the users.
     */
+
+  
     public HashMap<String, String> getUser(Connection conn, String tableName){
         
         Statement statement;
@@ -118,6 +149,7 @@ public class Database  {
     
     
     /**
+    * @lang it
     * @brief Verifica le credenziali di login di un utente.
     * 
     * @param conn Oggetto Connection per interagire con il database.
@@ -126,7 +158,18 @@ public class Database  {
     * @param password Password inserita dall'utente.
     * @return 1 se le credenziali sono corrette, 0 se la password è errata, -1 se l'email non esiste.
     * @throws SQLException Se si verifica un errore durante l'interrogazione.
+    * @lang en
+    * 
+    * @brief Verifies the login credentials of a user.
+    * 
+    * @param conn Connection object to interact with the database.
+    * @param tableName Name of the table containing the users.
+    * @param email Email of the user.
+    * @param password Password entered by the user.
+    * @return 1 if the credentials are correct, 0 if the password is incorrect, -1 if the email does not exist.
+    * @throws SQLException If an error occurs during the query.
     */
+
     
     public int checkLogin(Connection conn,String tableName, String email, String password) throws SQLException{
     
@@ -160,11 +203,19 @@ public class Database  {
     }
     
     /**
-    * @brief Cripta la password passata all' utente 
+    * @lang it
+    * @brief Cripta la password passata dal utente 
     * 
     * @param password è la password passata alla funzione che verra criptata
     * @return  Una stringa criptata
+    * @lang en
+    * 
+    * @brief Encrypts the password provided by the user.
+    * 
+    * @param password The password passed to the function to be encrypted.
+    * @return An encrypted string.
     */
+
     
     private static String hashPassword(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt());
@@ -172,12 +223,21 @@ public class Database  {
     }
     
     /**
+    * @lang it
     * @brief Verifica se le 2 password corrispondono 
     * 
     * @param password è la password non criptata passata alla funzione 
     * @param hashed è la password criptata passata alla funzione 
     * @return  true se la password è uguale a hashed, se sono diverse false
+    * @lang en
+    * 
+    * @brief Verifies if the two passwords match.
+    * 
+    * @param password The unencrypted password passed to the function.
+    * @param hashed The encrypted password passed to the function.
+    * @return true if the password matches the hashed password, false otherwise.
     */
+
 
     private static boolean checkPassword(String password, String hashed) {
         return BCrypt.checkpw(password, hashed);
@@ -187,13 +247,23 @@ public class Database  {
     
     
     /**
+    * @lang it
     * @brief Recupera i contatti associati a un utente specifico.
     * 
     * @param conn Oggetto Connection per interagire con il database.
     * @param tableName Nome della tabella contenente i contatti.
     * @param email Email dell'utente di cui recuperare i contatti.
     * @return HashMap contenente i contatti dell'utente.
+    * @lang en
+    * 
+    * @brief Retrieves the contacts associated with a specific user.
+    * 
+    * @param conn Connection object to interact with the database.
+    * @param tableName Name of the table containing the contacts.
+    * @param email Email of the user whose contacts are to be retrieved.
+    * @return HashMap containing the user's contacts.
     */
+
     
    public HashMap<String, Contact> getContact(Connection conn, String tableName,String email){
         
@@ -223,6 +293,7 @@ public class Database  {
     }
     
     /**
+    * @lang it 
     * @brief Crea il contatto partendo dai parametri passati.
     * 
     * @param name nome del contatto.
@@ -232,7 +303,17 @@ public class Database  {
     * @param em_cont stringa contenete l' emails del contatto con dei separatori.
     * @param ID contiene l' identificativo univoco del contatto
     * @return Contact contenente con tutti i parametri passati.
-    */
+    * @lang en
+    * @brief Creates a contact from the given parameters.
+    * 
+    * @param name Name of the contact.
+    * @param surname Surname of the contact.
+    * @param numeri String containing the contact's numbers with separators.
+    * @param tag String containing the contact's tags with separators.
+    * @param em_cont String containing the contact's emails with separators.
+    * @param ID Contains the unique identifier of the contact.
+    * @return Contact containing all the passed parameters.
+ */
 
    private Contact createContact(String name, String surname, String numeri, String tag, String em_cont,String ID) {
         Contact c= new Contact(name,surname, Integer.valueOf(ID));
@@ -269,15 +350,24 @@ public class Database  {
     }
     
     /**
-     * 
+    * @lang it 
     * @brief Inserisce il contatto associato ad un utente specifico.
     * 
     * @param cont contatto da aggiungere
     * @param conn Oggetto Connection per interagire con il database.
     * @param tableName Nome della tabella contenente i contatti.
     * @param email_Utente Email dell'utente che ha fatto il login.
-    *  @throws SQLException Se si verifica un errore durante l'interrogazione.
+    * @throws SQLException Se si verifica un errore durante l'interrogazione.
+    * @lang en
+    * @brief Inserts the contact associated with a specific user.
+    * 
+    * @param cont Contact to be added.
+    * @param conn Connection object to interact with the database.
+    * @param tableName Name of the table containing the contacts.
+    * @param email_Utente Email of the user who logged in.
+    * @throws SQLException If an error occurs during the query.
     */
+
     
      public void insertContact(Connection conn, String tableName, Contact cont, String email_Utente) throws SQLException{
     
@@ -313,10 +403,16 @@ public class Database  {
     }
     
     /**
+    * @lang it 
     * @brief Permette di impostare un ArrayList su una strinca aggiungendo un separatore specifico.
     * @param s ArrayList da formattare.
     * @return Una stringa con tutti i valori dis separati da un separatore
-    */
+    * @lang en
+    * @brief Allows setting an ArrayList to a string by adding a specific separator.
+    * @param s ArrayList to format.
+    * @return A string with all values separated by a separator.
+ */
+
     private String formattaOut(ArrayList<String> s) {
         
         String formattata="";
@@ -328,14 +424,23 @@ public class Database  {
     }
     
     /**
+    * @lang it
     * @brief Modifica il contatto associato a un utente specifico nel Database.
-    * 
     * @param conn Oggetto Connection per interagire con il database.
     * @param tableName Nome della tabella contenente i contatti.
     * @param cont il contatto da modificare nel Database
     * @param email_Utente Email dell'utente che ha fatto il login.
-    *  @throws SQLException Se si verifica un errore durante l'interrogazione.
+    * @throws SQLException Se si verifica un errore durante l'interrogazione.
+    * @lang en
+    * @brief Modifies the contact associated with a specific user in the database.
+    * 
+    * @param conn Connection object to interact with the database.
+    * @param tableName Name of the table containing the contacts.
+    * @param cont The contact to be modified in the database.
+    * @param email_Utente Email of the user who logged in.
+    * @throws SQLException If an error occurs during the query.
     */
+
     public void modifyContact(Connection conn, String tableName, Contact cont, String email_Utente) throws SQLException{
         
          Statement statment;
@@ -367,7 +472,16 @@ public class Database  {
       statment.executeUpdate(query);
       System.out.println("Contatto modificato");
     }
-    public void CloseConnection(Connection conn){
+    public void RemuveContactByID(Connection conn, String tableName, String ID) throws SQLException{
     
+       Statement statment;
+       
+       String query= String.format("delete from %s where ID='%s'",tableName,ID);
+       statment= conn.createStatement();
+       statment.execute(query);
+       System.out.print("\n Dato eliminato per ID");
+    }
+    public void CloseConnection(Connection conn) throws SQLException{
+        conn.close();
     }
 }
